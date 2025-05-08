035d14f3b5f354dfe5db08455a8a3d1b059a1fe9
ehsan <1374ea@gmail.com>
2025-05-08 17:33:45 +0330

    Step 35

--- apps/service_1/internal/helpers/logger.go
+++ apps/service_1/internal/helpers/logger.go
@@ -11,11 +11,9 @@ func SetupLogger(cfg *Configs, processName string) {
 	if cfg.IsDebug {
 		switchToHumanReadableMode()
 	}
-	log.Logger = log.Logger.With(). //nolint:reassign // Library's suggested way
-					Str("process_name", processName).
-					Logger()
+	log.Logger = log.Logger.With().Str("process_name", processName).Logger()
 }
 
 func switchToHumanReadableMode() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}) //nolint:reassign // Library's suggested way
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 }
